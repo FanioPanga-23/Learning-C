eec4b6991b8b57cbbb8ad8eb95a34b74cbfba903
f-99 <faniospanga@gmail.com>
2025-05-13 14:26:20 +0100

    New changes

--- Cods/Recap/capt6_Vetores/exam/ex04/prog0609.c
+++ Cods/Recap/capt6_Vetores/exam/ex04/prog0609.c
@@ -5,11 +5,133 @@
  * ******************************************************/
 
 #include <stdio.h>
+#include <unistd.h>
 #define	DIM 3
 #define SPACE ' '
 #define BOLA 'O'
 #define	X 'X'
 
+/*Conta o tamanho de qualquer string*/
+int	ft_strlen(char *s)
+{
+	int	i;
+
+	i = 0;
+	while (s[i] != '\0')
+		i++;
+	return (i);
+}
+
+/*Mensagem com o titulo do jogo*/
+void	ft_putGameName(void)
+{
+	printf("======== JOGO DA VELHA ========\n");
+}
+
+void	ft_clearBuffer(void)
+{
+	char	ch;
+
+	while ((ch = getchar()) != '\n' && ch != EOF)
+		;
+	return ;
+}
+
+/*Verifica se um caractere pertence ao alfabeto*/
+int	ft_isAlpha(char ch)
+{
+	return (ch >= 'a' && ch <= 'z' || ch >= 'A' && ch <= 'Z');
+}
+
+/*Verifica o nome insirido, so aceita caracteres do alfabeto*/
+int	ft_checkName(char *s)
+{
+	int	i;
+
+	i = 1;
+	while (s[i] != '\0')
+	{
+		if ( ! ft_isAlpha(s[i]) )
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
+/*Mensagem de erro quando o padrao do nome do jogador estiver errado*/
+void	ft_ERROR_PlName(void)
+{
+	puts("ERRO: O nome deve conter apenas caracteres do alfabeto!!");
+	return ;
+}
+
+/*Verifica se os nomes dos jogadores sao iguais*/
+int	ft_isTheSame(char *s1, char *s2)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	j = 0;
+	while (s2[i] != '\0')
+	{
+		if (s1[i] != s1[j])
+			return (0);
+		i++;
+		j++;
+	}
+	return (1);
+}
+
+/*Adiciona um digito no final do nome do Player2 quando os
+ * nomes forem iguais*/
+void	*ft_addDif(char *s)
+{
+	int	i;
+	int	length;
+
+	length = ft_strlen(s);
+	s[length] = '1';
+	s[length+1] = '\0';
+}
+
+/*Faz a leitura dos nomes dos jogadores*/
+void	*ft_getPlayerName(char *player1, char *player2)
+{
+	int	cont;
+
+	cont = 1;
+	while (1)
+	{
+		printf("Jogador %d: ", cont);
+		scanf("%s", player1);
+		if ( ! ft_checkName(player1) )
+		{
+			ft_ERROR_PlName();
+			ft_clearBuffer();
+			continue;
+		}
+		else
+		{
+			printf("Jogador %d: ", cont + 1);
+			scanf("%s", player2);
+			if ( ! ft_checkName(player2) )
+			{
+				ft_ERROR_PlName();
+				ft_clearBuffer();
+				continue;
+			}
+			else
+			{
+				if (ft_isTheSame(player1, player2))
+					ft_addDif(player2);
+				break;
+			}
+		}
+	}
+}
+
+/*Inicia o tabuleiro*/
 void	ft_startTab(char v[DIM][DIM])
 {
 	int	i;
@@ -21,13 +143,14 @@ void	ft_startTab(char v[DIM][DIM])
 		j = 0;
 		while (j < DIM)
 		{
-			v[i][j] = BOLA;
+			v[i][j] = SPACE;
 			j++;
 		}
 		i++;
 	}
 }
 
+/*Mostra o tabuleiro*/
 void	ft_showTab(char v[DIM][DIM])
 {
 	int	i;
@@ -55,59 +178,45 @@ void	ft_showTab(char v[DIM][DIM])
 	}
 }
 
+/*Verifica situacao de vitoria*/
 int	ft_checkWin(char v[DIM][DIM])
 {
 	int	retorno;
 
 	retorno = 0;
-	/*Primeira Linha*/
-	if (v[0][0] == X && v[0][1] == X && v[0][2] == X)
-		retorno++;
-	if (v[0][0] == BOLA && v[0][1] == BOLA && v[0][2] == BOLA)
-		retorno++;
-	/*Segunda Linha*/
-	if (v[1][0] == X && v[1][1] == X && v[1][2] == X)
-		retorno++;
-	if (v[1][0] == BOLA && v[1][1] == BOLA && v[1][2] == BOLA)
-		retorno++;
-	/*Terceira Linha*/
-	if (v[2][0] == X && v[2][1] == X && v[2][2] == X)
-		retorno++;
-	if (v[2][0] == BOLA && v[2][1] == BOLA && v[2][2] == BOLA)
-		retorno++;
-	/*Primeria Coluna*/
-	if (v[0][0] == X && v[1][0] == BOLA && v[2][0] == X)
-		retorno++;
-	if (v[0][0] == BOLA && v[1][0] == BOLA && v[2][0] == BOLA)
-		retorno++;
-	/*Segunda Coluna*/
-	if (v[0][1] == X && v[1][1] == X && v[2][1] == X)
-		retorno++;
-	if (v[0][1] == BOLA && v[1][1] == BOLA && v[2][1] == BOLA)
-		retorno++;
-	/*Terceira Coluna*/
-	if (v[0][2] == X && v[1][2] == X && v[2][2] == X)
-		retorno++;
-	if (v[0][2] == BOLA && v[1][2] == BOLA && v[2][2] == BOLA)
-		retorno++;
-	/*Diagonal Principal*/
-	if (v[0][0] == X && v[1][1] == X && v[2][2] == X)
-		retorno++;
-	if (v[0][0] == BOLA && v[1][1] == BOLA && v[2][2] == BOLA)
-		retorno++;
-	/*Diagonal Secundaria*/
-	if (v[0][2] == X && v[1][1] == X && v[2][0] == X)
-		retorno++;
-	if (v[0][2] == BOLA && v[1][1] == BOLA && v[2][0] == BOLA)
-		retorno++;
-
+	/**/
 	return (retorno);
+}
+
+/*Verifica se o tabuleiro esta cheio*/
+int	ft_isFull(char v[DIM][DIM])
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	while (i < DIM)
+	{
+		j = 0;
+		while (j < DIM)
+		{
+			if (v[i][j] == SPACE)
+				return (0);
+			j++;
+		}
+		i++;
+	}
+	return (1);
 }
 
 int	main(void)
 {
 	char	tabuleiro[DIM][DIM];
+	char	player1[20] = "";
+	char	player2[20] = "";
 
 	ft_startTab(tabuleiro);
 	ft_showTab(tabuleiro);
+	ft_getPlayerName(player1, player2);
+	printf("Jogador 1 -> %s\nJogador 2 -> %s\n", player1, player2);
 }
